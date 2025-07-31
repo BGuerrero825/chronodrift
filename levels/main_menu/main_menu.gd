2fb6287f0a2dfd603eb47e35a5113883471b5290
babu <bguerrero825@gmail.com>
2025-07-31 08:50:02 -0500

    salvage from fly-out, pare down from here;

---
+++ levels/main_menu/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready() -> void:
+	HUD.update_speedometer(0, true)
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/options_menu/options_menu.tscn")
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/play_menu/play_menu.tscn")
+
