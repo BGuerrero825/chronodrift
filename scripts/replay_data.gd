0d280cfb3d931ae16c741b206f134c25df9311d6
Wes-Ray <wesleyray95@gmail.com>
2025-08-02 09:39:19 -0700

    added throttle and current_speed data to tracker

--- scripts/replay_data.gd
+++ scripts/replay_data.gd
@@ -5,8 +5,13 @@ extends Resource
 
 @export var position_tracker: PackedVector3Array = []
 @export var transform_tracker: Array[Transform3D] = []
+@export var throttle_tracker: Array[float] = []
+@export var speed_tracker: Array[float] = []
 
 func record_frame(player_ref: PlayerShip) -> void:
     assert(player_ref != null, "attempting to record when no player_ref is set")
     position_tracker.append(player_ref.global_position)
     transform_tracker.append(player_ref.global_transform)
+    throttle_tracker.append(player_ref.throttle)
+    speed_tracker.append(player_ref.current_speed)
+
